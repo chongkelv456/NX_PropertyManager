8be03bb17ca980526f338a41a8f1e71a46e194f1
chongkelv <e0895806@u.nus.edu>
2024-05-15 16:44:34 +0800

    Add Plate class

---
+++ PropertiesManager/Model/Hardness.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesManager.Model
+{
+    public class Hardness
+    {
+        private List<string> hardnesses = new List<string>();
+
+        public Hardness()
+        {
+            hardnesses.Add("-");
+            hardnesses.Add("35~40");
+            hardnesses.Add("52~54");
+            hardnesses.Add("57~59");
+            hardnesses.Add("58~60");
+            hardnesses.Add("60~63");
+            hardnesses.Add("62~65");
+
+        }
+    }
+}
---
+++ PropertiesManager/Model/Plate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesManager.Model
+{
+    public class Plate
+    {
+        private List<string> plates = new List<string>();
+
+        public Plate()
+        {
+            plates.Add("UPPER PAD SPACER");
+            plates.Add("UPPER PAD");
+            plates.Add("PUNCH HOLDER");
+            plates.Add("BOTTOMING PLATE");
+            plates.Add("STRIPPER PLATE");
+            plates.Add("MATERIAL GUIDE-F/R");
+            plates.Add("MATERIAL GUIDE-F");
+            plates.Add("MATERIAL GUIDE-R");
+            plates.Add("DIE PLATE");
+            plates.Add("DIE PLATE-R");
+            plates.Add("DIE PLATE-F");
+            plates.Add("LOWER PAD");
+            plates.Add("LOWER PAD SPACER");
+        }
+    }
+}
